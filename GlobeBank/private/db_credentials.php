de37d1a5cf02cde1fc36e7e46472c188f3d36498
Addison Jones <addison.jones3@gmail.com>
2017-10-26 09:49:00 -0400

    Update db_credentials.php

--- GlobeBank/private/db_credentials.php
+++ GlobeBank/private/db_credentials.php
@@ -2,7 +2,7 @@
 
   define('DB_SERVER', 'localhost');
   define('DB_USER', 'webuser');
-  define('DB_PASS', 's1lversonic');
+  define('DB_PASS', 'mypassword');
   define('DB_NAME', 'globe_bank');
 
  ?>
